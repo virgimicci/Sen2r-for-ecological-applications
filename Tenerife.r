438d8dcf765ce89780a04df7d953fb9546424a85
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2021-03-16 11:59:29 +0100

    Create Tenerife.r

---
+++ Tenerife.r
@@ -0,0 +1,15 @@
+install.packages("sen2r")
+library(sen2r)
+
+# Error: 
+# Some missing packages are needed to run the GUI; please install them with
+# the command
+# > install.packages(c("leaflet", "leafpm", "mapedit", "shiny", "shinyFiles", "shinydashboard", "shinyjs", "shinyWidgets")) 
+
+install.packages(c("leaflet", "leafpm", "mapedit", "shiny", "shinyFiles", "shinydashboard", "shinyjs", "shinyWidgets"))
+
+install.packages("remotes")
+library(remotes)
+library(raster)
+library(sp)
+
