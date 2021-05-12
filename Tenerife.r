c5988135f978eca42d372b00cc1798bd9cf9e0e4
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2021-05-12 22:17:12 +0200

    Update Tenerife.r

--- Tenerife.r
+++ Tenerife.r
@@ -63,7 +63,7 @@ shp <- st_transform(myextent, CRS("+proj=utm +zone=28 +datum=WGS84 +units=m +no_
 tf_2111ndvi <- mask( tf_2111ndvi, shp)
 plot(tf_2111ndvi)
 
-set.seed(95)
+set.seed(42)
 cl <- viridis(6)
 
 #tenerife
@@ -73,22 +73,33 @@ plot(tenerife$map, col=cl)
 
 # anaga
 extent <- c( 370000, 390560, 3140000, 3163080 )
-anagandvi <- crop(tf_2111ndvi, extent)
-anagandvic <- unsuperClass(anagandvi, nClasses = 6)
+anagandvi <- crop(tenerifer, extent)
+anagandvic <- unsuperClass(anagandvi, nClasses = 4)
 
-
-plot(anagandvic$map, col=cl)
+plot(anagandvic$map, col=cl, main = "Anaga piani vegetazionali")
 
 par(mfrow=c(1,2))
 plot(anagandvic$map, col=cl, main = "unsupervised classification (6)")
 plot(anagandvi, main= "NDVI")
 
+## teide
+extent2 <- c(330000, 355000, 3117000, 3135000)
+teidendvi <- crop(tenerifer, extent2)
+teidendvic <- unsuperClass(teidendvi, nClasses = 3)
+plot(teidendvic$map, col=cl, main = "Teide piani vegetazionali")
 
-# Rstoolbox::unsuperClass
-anagac <- unsuperClass(anaga, nClasses = 7)
-cl <- colorRampPalette(c('yellow','black','red'))(100)
-plot(anaga$map, col = cl)
+## teno
 
+plot(tenerifer)
+extent3 <- c(310000, 330000, 3130000, 3145000)
+tenondvi <- crop(tenerifer, extent3)
+tenondvic <- unsuperClass(tenondvi, nClasses = 4)
+plot(tenondvic$map, col=cl, main = "Teide piani vegetazionali")
+
+par(mfrow=c(1,3))
+plot(tenondvic$map, col=cl, main = "Teide piani vegetazionali")
+plot(anagandvic$map, col=cl, main = "Anaga piani vegetazionali")
+plot(tenerife$map, col=cl)
 
 ###################
 # tenerife marzo-aprile-maggio # nuvolenuvolenuvoleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee :/
