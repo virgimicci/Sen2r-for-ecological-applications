6acf80c3fa60775a0cace969b1dba6169d2e7088
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2021-06-09 12:20:21 +0200

    Update RStoolbox::unsuperClass.r

--- RStoolbox::unsuperClass.r
+++ RStoolbox::unsuperClass.r
@@ -7,6 +7,7 @@ library(viridis)
 
 Jan <-  ndvi_m$NDVIJan20
 Nov <- ndvi_m$NDVINov01
+May <- ndvi_m$NDVIMay20
 Jan1 <-  ndwi_m$NDWIJan20
 Nov1 <- ndwi_m$NDWINov01
 
@@ -20,20 +21,27 @@ show_col(viridis_pal()(20))
 
 #tenerife Jan NDVI
 Janr <- aggregate(Jan, fact= 10)
+Janr20 <- aggregate(Jan, fact= 20)
 Janr_cl <-  unsuperClass(Janr, nClasses = 6)
-plot(Janr_cl$map, col=cl, main= "Unsupervised classification Jan", axes= FALSE)
-#440154FF #404788FF #287D8EF #3CBB75FF # 95D840FF # FDE725FF
+Janr_cl20 <-  unsuperClass(Janr, nClasses = 6)
+plot(Janr_cl20$map, col=cl, main= "Unsupervised classification Jan", axes= FALSE)
+
+#tenerife May NDVI
+Mayr20 <- aggregate(May, fact= 20)
+Mayr_cl20 <-  unsuperClass(Mayr, nClasses = 6)
+plot(Mayr_cl20$map, col=cl, main= "Unsupervised classification May", axes= FALSE)
+
+
+#tenerife nov NDVI
+Novr <- aggregate(Nov, fact= 20)
+Novr_cl20 <-  unsuperClass(Novr, nClasses = 6)
+plot(Novr_cl20$map, col=cl, main= "Unsupervised classification Nov", axes= FALSE)
+
 
 #tneerife Jan NDWI
 Jan1r <- aggregate(Jan1, fact= 10)
 Jan1r_cl <-  unsuperClass(Jan1r, nClasses = 6)
 plot(Jan1r_cl$map, col=cl, main= "Unsupervised classification Jan", axes= FALSE)
-
-#tenerife nov
-Novr <- aggregate(Nov, fact= 10)
-Novr_cl <-  unsuperClass(Novr, nClasses = 6)
-plot(Novr_cl$map, col=cl, main= "Unsupervised classification Nov", axes= FALSE)
-
 # anaga
 extent <- c( 370000, 390560, 3140000, 3163080 )
 
